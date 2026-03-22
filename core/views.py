ae60e460cdd0f6c41ea1e2237703073bbce623a6
pushpitshukla28 <pushpitshukla28@gmail.com>
2026-03-23 00:13:15 +0530

    Fix 500 on friends search: import User model in views.py

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
